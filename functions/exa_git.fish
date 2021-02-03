1a35164bfb6cef2448aa1a848e70158664ff5cb6
graham <graham.annett@gmail.com>
2021-02-03 10:31:01 -0800

    Update exa_git.fish

--- functions/exa_git.fish
+++ functions/exa_git.fish
@@ -1,5 +1,5 @@
 function exa_git -d "Use exa and it's --git option if in a git repo"
-    if git rev-parse --is-inside-work-tree &>/dev/null
+    if git rev-parse --is-inside-work-tree >/dev/null 2>&1
         exa $EXA_STANDARD_OPTIONS {$EXA_LL_OPTIONS} --git $argv
     else
         exa $EXA_STANDARD_OPTIONS {$EXA_LL_OPTIONS} $argv
